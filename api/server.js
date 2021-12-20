0e89f60685123abba731176820b7ef1f6744aef0
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-12-20 18:33:48 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -8,10 +8,21 @@ const session = require("express-session")
 
 const server = express();
 
+// const itemsRouter = require("../items/items-router.js")
+const userRouter = require("../users/users-router.js")
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+// server.use(session({
+//   resave:false, 
+//   saveUninitialized: false, 
+//   secret: process.env.JWT_SECRET, 
+// }))
+
+
+// server.use("/items", itemsRouter)
+server.use("/", userRouter)
 
 server.get("/api", (req, res) => {
   res.json({ message: "Your API is up and running" });
