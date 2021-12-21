57f1093837a948c0d35ba2fcffa22d7d3cac7c1f
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-12-20 20:51:35 -0500

    heroku push

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ const session = require("express-session")
 const server = express();
 
 // const itemsRouter = require("../items/items-router.js")
-const userRouter = require("../users/users-router.js")
+//const userRouter = require("../users/users-router.js")
 
 server.use(helmet());
 server.use(cors());
@@ -22,7 +22,7 @@ server.use(express.json());
 
 
 // server.use("/items", itemsRouter)
-server.use("/", userRouter)
+//server.use("/", userRouter)
 
 server.get("/api", (req, res) => {
   res.json({ message: "Your API is up and running" });
