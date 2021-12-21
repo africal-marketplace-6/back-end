b06ce794d812e9ba19792012a9a52ffb360cf7b7
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-12-20 21:10:51 -0500

    heroku test

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
 
 const pg = require('pg')
-// if(process.env.DATABASE_URL){
-//   pg.defaults.ssl = { rejectUnauthorized: false }
-// }
-
+if (process.env.DATABASE_URL) {
+pg.defaults.ssl = { rejectUnauthorized: false }
+}
 const sharedConfig = {
   client: 'pg',
   migrations: { directory: './api/data/migrations' },
