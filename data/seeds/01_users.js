7020dbf4d8ab6c528a4ecaed341433ea9dd86535
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-12-20 22:05:59 -0500

    heroku test 3

--- data/seeds/01_users.js
+++ data/seeds/01_users.js
@@ -7,7 +7,7 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {username: 'test', password: hashedPassword, email: "1234@1234.com"}
+        {user_id: 1, username: 'test', password: hashedPassword, email: "1234@1234.com"}
       
       ]);
     });
